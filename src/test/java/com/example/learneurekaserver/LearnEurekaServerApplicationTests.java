1d0c51dbe96fb574deae134e1c8ec5ece6dd2ee1
isarthak <sarthakmehta649@gmail.com>
2024-10-21 08:06:36 +0530

    Eureka Server App

---
+++ src/test/java/com/example/learneurekaserver/LearnEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learneurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
